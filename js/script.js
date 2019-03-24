82e1c8d36a7911f563965c0eb3e22ed1fffe6cca
Anton <antonliashok@gmail.com>
2019-03-24 14:02:18 +0200

    clear: html and js

--- js/script.js
+++ js/script.js
@@ -25,19 +25,3 @@
 		setTimeout(function() {
 			clearInterval(timerId);
 		}, 1750);}
-
-	function counter (element) {
-		element.innerHTML = "nice night!!!";
-	}
-
-	var div = document.createElement('div');
-	var textElem = document.createTextNode('Тут был я');
-
-function messege(){
-  var div = document.createElement('div');
-  div.className = "alert alert-success";
-  div.innerHTML = "<strong>Ура!</strong> Вы прочитали это важное сообщение.";
-
-  document.body.appendChild(div);
-
-};
