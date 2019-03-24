b0c7d223a0534636dedf593e4c059b6c1d214e68
Anton <antonliashok@gmail.com>
2019-03-24 14:21:54 +0200

    add: event listener

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
+	window.onload = function() {
+		const btn1 = document.querySelector('.red');
+		const btn2 = document.querySelector('.yellow');
+		const btn3 = document.querySelector('.green');
+
+		btn1.addEventListener('click', toggleRed);
+		
+
+	}
+
 	function toggleRed() {
 
 		var timerId = setInterval(function() {
-		  	document.getElementById("red-circle").classList.toggle("opacity-red");
+			document.querySelector('.red').classList.toggle("opacity-red");
 		}, 250);
 		setTimeout(function() {
 			clearInterval(timerId);
 		}, 1750);
 	}
 
-	function toggleYellow() {
-		var timerId = setInterval(function() {
-		  	document.getElementById("yellow-circle").classList.toggle("opacity-yellow");
-		}, 250);
-		setTimeout(function() {
-			clearInterval(timerId);
-		}, 1750);
-	}
+	// function toggleYellow() {
+	// 	var timerId = setInterval(function() {
+	// 	  	document.getElementById("yellow-circle").classList.toggle("opacity-yellow");
+	// 	}, 250);
+	// 	setTimeout(function() {
+	// 		clearInterval(timerId);
+	// 	}, 1750);
+	// }
 
-	function toggleGreen() {
-		var timerId = setInterval(function() {
-		  	document.getElementById("green-circle").classList.toggle("opacity-green");
-		}, 250);
-		setTimeout(function() {
-			clearInterval(timerId);
-		}, 1750);}
+	// function toggleGreen() {
+	// 	var timerId = setInterval(function() {
+	// 	  	document.getElementById("green-circle").classList.toggle("opacity-green");
+	// 	}, 250);
+	// 	setTimeout(function() {
+	// 		clearInterval(timerId);
+	// 	}, 1750);}
